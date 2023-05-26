bdef70745e461b26dc64ec334a1a0b4041f2ae87
sol <ocosio0907@gmail.com>
2023-05-26 12:35:21 +0900

    receive todo data

--- backend/main.go
+++ backend/main.go
@@ -10,20 +10,19 @@ import (
 type todo struct {
 	ID    float64 `json:"id"`
 	Title string  `json:"title"`
-	Text  string  `json:"text"`
 }
 
 var todos = []todo{
-	{ID: 1, Title: "first todo", Text: "first todo"},
+	{ID: 1, Title: "first todo"},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/todo", getTodo)
+	router.POST("/todo", postTodo)
 
 	router.Run("localhost:8080")
 	fmt.Println("Starting server at port 8080")
-	fmt.Println("hello world")
 }
 
 func getTodo(c *gin.Context) {
@@ -33,4 +32,20 @@ func getTodo(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 	c.IndentedJSON(http.StatusOK, todos)
+}
+
+func postTodo(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+	var newTodoItem todo
+
+	if err := c.BindJSON(&newTodoItem); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodoItem)
+	c.IndentedJSON(http.StatusCreated, newTodoItem)
 }
